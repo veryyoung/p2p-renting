c75f9a9c163837ea7c8110a63934a0a6f240f035
veryyoung <codingyoung@gmail.com>
2015-10-25 00:53:05 +0800

    解决启动异常

--- backend/src/main/java/gg/sf/renting/dao/ImageDao.java
+++ backend/src/main/java/gg/sf/renting/dao/ImageDao.java
@@ -1,14 +1,16 @@
 package gg.sf.renting.dao;
 
 import gg.sf.renting.entity.Image;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by veryyoung on 2015/10/24.
  */
+@Repository
 public class ImageDao extends BaseDao<Image> {
 
-    public ImageDao(Class<Image> entityClass) {
-        super(entityClass);
+    public ImageDao(){
+        super(Image.class);
     }
 
 
