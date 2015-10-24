a58ec353b59b110e2ce766c6c7d41d301929a8bb
veryyoung <codingyoung@gmail.com>
2015-10-24 22:04:08 +0800

    add 7niu utils

---
+++ backend/src/main/java/gg/sf/renting/entity/Image.java
@@ -0,0 +1,23 @@
+package gg.sf.renting.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * Created by veryyoung on 2015/10/24.
+ */
+@Data
+@Entity
+@Table(name = "image")
+public class Image {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    @Column(length = 32)
+    private String id;
+
+    private String roomId;
+}
