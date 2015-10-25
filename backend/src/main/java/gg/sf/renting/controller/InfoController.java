4e5c0b4dc591ddbaf5a2416ae4208a6fd25087fd
veryyoung <codingyoung@gmail.com>
2015-10-25 08:05:03 +0800

    发布信息校验 token

--- backend/src/main/java/gg/sf/renting/controller/InfoController.java
+++ backend/src/main/java/gg/sf/renting/controller/InfoController.java
@@ -4,8 +4,10 @@ import gg.sf.renting.entity.LandlordInfo;
 import gg.sf.renting.entity.RenterInfo;
 import gg.sf.renting.rest.RestData;
 import gg.sf.renting.service.InfoService;
+import gg.sf.renting.service.TokenService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,28 +21,39 @@ public class InfoController {
     @Autowired
     private InfoService infoService;
 
+    @Autowired
+    private TokenService tokenService;
+
     @RequestMapping("publish/renter")
-         public RestData publish(RenterInfo renterInfo) {
+    public RestData publish(RenterInfo renterInfo, @RequestHeader(value = "token", required = false) String token) {
         RestData restData = new RestData();
-        String infoId = infoService.addRenterInfo(renterInfo);
-        if(StringUtils.isNoneEmpty(infoId)){
-            restData.setSuccess(1);
-            restData.setComment("信息发布成功");
-        }else {
-            restData.setComment("发布失败，请校验数据格式");
+        if (tokenService.checkToken(token)) {
+            String infoId = infoService.addRenterInfo(renterInfo);
+            if (StringUtils.isNoneEmpty(infoId)) {
+                restData.setSuccess(1);
+                restData.setComment("信息发布成功");
+            } else {
+                restData.setComment("发布失败，请校验数据格式");
+            }
+        } else {
+            restData.setComment("token验证失败");
         }
         return restData;
     }
 
     @RequestMapping("publish/landlord")
-    public RestData publishLand(LandlordInfo landlordInfo) {
+    public RestData publishLand(LandlordInfo landlordInfo, @RequestHeader(value = "token", required = false) String token) {
         RestData restData = new RestData();
-        String infoId = infoService.addLandInfo(landlordInfo);
-        if(StringUtils.isNoneEmpty(infoId)){
-            restData.setSuccess(1);
-            restData.setComment("信息发布成功");
-        }else {
-            restData.setComment("发布失败，请校验数据格式");
+        if (tokenService.checkToken(token)) {
+            String infoId = infoService.addLandInfo(landlordInfo);
+            if (StringUtils.isNoneEmpty(infoId)) {
+                restData.setSuccess(1);
+                restData.setComment("信息发布成功");
+            } else {
+                restData.setComment("发布失败，请校验数据格式");
+            }
+        } else {
+            restData.setComment("token验证失败");
         }
         return restData;
     }
